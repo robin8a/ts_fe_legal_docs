66988183905b1266fafacf1bcb034547ef023284
Robin Ochoa <robin8a@gmail.com>
2026-05-08 16:42:24 -0500

    amplify pulled

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "tsbelegaldocsapi": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
